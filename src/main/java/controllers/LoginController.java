d55a2baa3c463d134cc848a22738da82de6544e6
Alejandro <alejandrocepeda2001@gmail.com>
2026-06-11 01:49:01 -0500

    Incorporacion de main menu

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -2,10 +2,18 @@ package controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import model.Persona.Admin.Administrador;
 import model.Persona.Admin.AdministradorDAO;
+import utils.Paths;
+
+import java.io.IOException;
 
 public class LoginController {
     @FXML
@@ -19,15 +27,30 @@ public class LoginController {
 
     @FXML
     void login_Administrador(ActionEvent event) {
-        validarLogin();
+        validarLogin(event);
     }
 
-    private void validarLogin(){
+    private void validarLogin(ActionEvent event){
         String user = txt_username.getText();
         String pass = txt_password.getText();
         Administrador admin = administradorDAO.validarLogin(user, pass);
         if (admin != null){
             System.out.println("Bienvenido" + admin.getNombres());
+            try {
+                // Cargar el menú principal
+                FXMLLoader loader = new FXMLLoader(getClass().getResource(Paths.MAIN_MENU));
+                Parent root = loader.load();
+
+                // Obtener el Stage actual desde el botón
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+                // Cambiar la escena
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
         }else {
             System.out.println("Credenciales incorrectas");
             limpiarCampos();
--- src/main/java/utils/Paths.java
+++ src/main/java/utils/Paths.java
@@ -2,4 +2,5 @@ package utils;
 
 public class Paths {
     public static final String LOGIN = "/login.fxml";
+    public static final String MAIN_MENU = "/main_menu.fxml";
 }
