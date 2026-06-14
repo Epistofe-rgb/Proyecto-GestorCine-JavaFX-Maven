a7c812751f7ed7df150e76864e1c574e0719ee3b
Alejandro <alejandrocepeda2001@gmail.com>
2026-06-14 00:40:41 -0500

    agregue el eskelo en MainMenuController

--- src/main/java/controllers/MainMenuController.java
+++ src/main/java/controllers/MainMenuController.java
@@ -1,5 +1,31 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
 public class MainMenuController {
+    @FXML
+    private Pane contenedorCentral;
+//756 x 661
+    @FXML
+    void cerrar_sesion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ventana_boleteria(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ventana_cliente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ventana_facturas(ActionEvent event) {
+
+    }
 
 }
