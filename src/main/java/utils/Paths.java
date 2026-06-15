6dc5315f83d0439ab32f6afa5ee681d8163d418f
Alejandro laptop <alejandrocepeda2001@gmail.com>
2026-06-15 07:31:39 -0500

    cambio de nombres correcion

--- src/main/java/utils/Paths.java
+++ src/main/java/utils/Paths.java
@@ -3,6 +3,6 @@ package utils;
 public class Paths {
     public static final String LOGIN = "/login.fxml";
     public static final String MAIN_MENU = "/main_menu.fxml";
-    public static final String BOLETERIA_MENU = "/boleteria_menu1.fxml";
-    public static final String CLIENTE_MENU = "/cliente_menu.fxml";
+    public static final String BOLETERIA_MENU = "/cliente_menu.fxml";
+    public static final String CLIENTE_MENU = "/boleteria_menu1.fxml";
 }
